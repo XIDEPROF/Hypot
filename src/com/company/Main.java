6e6b75ff1d57f4370fdf827c8f710c9a0b717242
Serge <78102903+MrXides@users.noreply.github.com>
2021-11-06 03:24:43 +0300

    Е-01
    Е

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+import static java.lang.Math.sqrt;
+
+public class Main {
+
+    public static void main(String[] args) {
+	double cathetus1=3,cathetus2=4,hypot;
+	hypot=sqrt(cathetus1+cathetus2)+(cathetus1+cathetus2);
+	System.out.print("Дина гиппотинузы равна "+hypot);
+    }
+}
